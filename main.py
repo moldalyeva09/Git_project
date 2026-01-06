325f473143dc99d0641121d92cfa8e74aff25f90
Peri <moldalyeva.peku@gmail.com>
2026-01-06 11:51:38 +0600

    2jany at koshtum

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print("Hello Worldf")
+print("Ailin")
+print("Aktan")
+# ssh-keygen -t ed25519 -C "moldalyeva09@gmail.com" paroldu kaira kaira suraboo uchun ssh
+"""
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+git branch -M main ---- butak vetka mainden taragan vetka
+git remote add origin git@github.com:moldalyeva09/Git_project.git ---
+ origin - kak peremennyi ssylka saktalat
+git push -u origin main
+"""
